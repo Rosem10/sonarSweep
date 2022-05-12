f7e3283ca9b75e982db1d5cd87106867b10b7a79
Rose Mullan <50704560+Rosem10@users.noreply.github.com>
2022-05-12 10:42:24 +0100

    Refactor to use first function to solve second function

--- index.js
+++ index.js
@@ -7,23 +7,14 @@ function sonarSweep(report) {
         }
     })
     return count
-   
 }
 
 function sonarSweepPt2(report) { 
-    let count = 0
-
-    report.map((item, index) => {
+    return sonarSweep(report.map((num, index) => {
          return report.slice(index, index + 3).reduce((total, current) => { 
             return total+= current
         })
-    }).filter((num) => num !== undefined).forEach((num, index, array) => { 
-        if (num > array[index - 1]) { 
-            count ++
-        }
-    })
-
-    return count
+    }).filter((num) => num !== undefined))
 }
 
 module.exports = {sonarSweep, sonarSweepPt2}
